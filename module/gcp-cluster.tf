5c1210db53f56178135fdbff530127380c3038f2
sgandeplli <sgandepalli@softility.com>
2025-04-23 15:48:42 +0530

    Create gcp-cluster.tf

---
+++ module/gcp-cluster.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  zone = var.region
+  project = "symbolic-pipe-457709-n9"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  deletion_protection = false
+
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.node_machine_type
+    disk_size_gb = 500
+  }
+
+  remove_default_node_pool = false
+}
